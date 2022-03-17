7f3d9ed2f50fe3449a66b728e769ea483118e99f
Sayuri <99312315+IT21238758@users.noreply.github.com>
2022-03-17 07:06:11 +0000

    student.h completed

--- Student.h
+++ Student.h
@@ -1,9 +1,14 @@
-class Student {
-  // private section
-  //    int studentId
-  //    name <- 20 charcters
+class Student 
+{
+
+// private section
+private:
+int studentID;
+char studentName[20];
   
-  // public section
-  //    assignDetails() method declaration
-  //    display() method declaration
+// public section
+public:
+void assignDetails(int no, char name[]);
+void display();
+  
 };
